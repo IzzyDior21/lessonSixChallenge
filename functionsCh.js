3474b76039f512d954b9891ae92b3d15013ebdfe
Izzy Dior <shop218916@gmail.com>
2025-02-23 11:59:38 -0600

    Created functionsCh file. verified age using a if else statement. 02/11/25 Challenge6

--- functionsCh.js
+++ functionsCh.js
@@ -1,4 +1,4 @@
-//1. Create a function that runs the following
+ //1. Create a function that runs the following
 //
 //2. Pass and age through an argument/parameter
 //
@@ -10,25 +10,25 @@
 
 // IF ELSE STATEMENT
 
-//let age = readline.question("What is your age? ")
+let age = readline.question("What is your age? ")
 
 
 
-//const verifyAge = (age) => {
-//    if (age >= 20) {
-//
-//        console.log("You an adult.");    
-//    
-//    } else if (age >= 13 && age <= 19) {
-//    
-//        console.log("You are a teenager.");    
-//    
-//    } else  {
-//    
-//        console.log("You are a child.");
-//    
-//    }
-//         
-//}
-//
-//verifyAge(15)
+const verifyAge = (age) => {
+    if (age >= 20) {
+
+        console.log("You an adult.");    
+    
+    } else if (age >= 13 && age <= 19) {
+    
+        console.log("You are a teenager.");    
+    
+    } else  {
+    
+        console.log("You are a child.");
+    
+    }
+         
+}
+
+verifyAge(15) //nothing but the Hive babi
