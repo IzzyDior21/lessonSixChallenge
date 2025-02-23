3012c858ac59c04e89340f39aa0e9071211f1332
Izzy Dior <shop218916@gmail.com>
2025-02-23 11:45:35 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# FunctionsChallengeSix
+# lessonSixChallenge
---
+++ functionsCh.js
@@ -0,0 +1,34 @@
+//1. Create a function that runs the following
+//
+//2. Pass and age through an argument/parameter
+//
+//
+//4. call (activate) the function  
+
+
+//const readline = require('readline-sync')
+
+// IF ELSE STATEMENT
+
+//let age = readline.question("What is your age? ")
+
+
+
+//const verifyAge = (age) => {
+//    if (age >= 20) {
+//
+//        console.log("You an adult.");    
+//    
+//    } else if (age >= 13 && age <= 19) {
+//    
+//        console.log("You are a teenager.");    
+//    
+//    } else  {
+//    
+//        console.log("You are a child.");
+//    
+//    }
+//         
+//}
+//
+//verifyAge(15)
